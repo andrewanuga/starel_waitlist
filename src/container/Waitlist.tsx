74a38ecf3365dbd5427ae99b227202977238f11d
ANDREW <adakoleandrew21@gmail.com>
2025-08-30 10:16:15 +0100

    CHANGE WARNING SIGNS finally

--- src/container/Waitlist.tsx
+++ src/container/Waitlist.tsx
@@ -345,7 +345,7 @@ const HomePage = () => {
               /> 
             )}
           </div>
-          <ToastContainer
+          <ToastContainer className={"z-[1000000000000000000000000000000000]"}
             position="bottom-center"
             autoClose={5000}
             hideProgressBar={true}
